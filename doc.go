3c835b22f5e6f4493ec4212ca198bc02cb35bb67
wgliang <liangcszzu@163.com>
2016-07-15 20:52:50 +0800

    fork from facebookgo and fix all test error in it
    fork from facebookgo and fix all test error in it

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package poetry provides various measuring instruments.
+//
+// This library is based on Coda Hale's original work:
+// https://github.com/codahale/poetry, and a fork of
+// https://github.com/rcrowley/go-poetry.
+package poetry
