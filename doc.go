0cbacfb7290186fcdc1b0249919b0688a489da7f
wangguoliang <liangcszzu@163.com>
2016-08-25 12:49:17 +0800

    remove all .go

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Package poetry provides various measuring instruments.
-//
-// This library is based on Coda Hale's original work:
-// https://github.com/codahale/poetry, and a fork of
-// https://github.com/rcrowley/go-poetry.
-package poetry
